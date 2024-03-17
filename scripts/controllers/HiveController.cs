00b88114b0c9da761d58efa5cf4a7a844fe06dec
Nathaniel Sunday <nathille333@gmail.com>
2024-03-17 10:31:07 -0500

    Sfx and hive retaliate

--- scripts/controllers/HiveController.cs
+++ scripts/controllers/HiveController.cs
@@ -10,18 +10,19 @@ public partial class HiveController : Node2D, IDamagable, IDamaging
     public static HiveController Instance { get; private set; }
 
     private AnimationPlayer _animPlayer;
+    private Area2D _area;
 
     public int Health { get; private set; } = 100;
     public int MaxHealth => 100;
     public int Damage => 5;
-    public float Cooldown => 2;
-    public float CooldownTimer => 2;
+    public float Cooldown => 2f;
+    public float CooldownTimer { get; private set; }
     public bool Ranged => false;
     public bool CanTargetMultiple => true;
 
     public void Attack(Node2D target)
     {
-        (target as IDamagable)?.InflictDamage(Damage);
+        (target.GetParent() as IDamagable)?.InflictDamage(Damage);
     }
 
     public void Attack(List<Node2D> targets)
@@ -30,6 +31,9 @@ public partial class HiveController : Node2D, IDamagable, IDamaging
         {
             Attack(target);
         }
+
+        _animPlayer.Play("Attack");
+        _animPlayer.Queue("Idle");
     }
 
     public void InflictDamage(int amount)
@@ -37,6 +41,15 @@ public partial class HiveController : Node2D, IDamagable, IDamaging
         Health -= amount;
 
         if (Health <= 0) Die();
+
+        if (CooldownTimer > 0f) return;
+
+        var enemies = _area.GetOverlappingBodies().ToList();
+        if (!enemies.Any()) return;
+
+        Attack(enemies);
+
+        CooldownTimer = Cooldown;
     }
 
     public void Die()
@@ -57,7 +70,15 @@ public partial class HiveController : Node2D, IDamagable, IDamaging
         _animPlayer = GetChildren().OfType<AnimationPlayer>().First();
 
         _animPlayer?.Play("Ready");
+
+        _area = GetChildren().OfType<Area2D>().First();
     }
+
+    public override void _Process(double delta)
+    {
+        CooldownTimer -= (float)delta;
+    }
+
 
     public override void _UnhandledInput(InputEvent @event)
     {
